db6dd8615cd8337582c16a3494d94dbcfe18a4bb
Wanderson Matheus <w.matheuspontesdev@gmail.com>
2025-04-09 14:27:48 -0300

    comecei a mexer no spring ,apenas dei run,criei uma classe controller e um rota

---
+++ AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Controller.java
@@ -0,0 +1,15 @@
+package com.Equipe1.AssinaturaDigital;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Primeira mensagem nessa rota";
+    }
+}
