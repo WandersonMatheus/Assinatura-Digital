d57517509f3ce79eb5c2ccb1656b2750c12233ad
Wanderson Matheus <w.matheuspontesdev@gmail.com>
2025-04-15 14:46:32 -0300

    fiz o repository das entidades cenario e termo

--- AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Cenario/CenarioRepository.java
+++ AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Cenario/CenarioRepository.java
@@ -1,5 +1,11 @@
 package com.Equipe1.AssinaturaDigital.Cenario;
 
-public interface CenarioRepository {
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface CenarioRepository extends MongoRepository<CenarioModel,String>{
 
+    Optional<CenarioModel> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<CenarioModel> findByNomeContainingIgnoreCase(String parteDoNome);
 }
--- AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Termo/TermoRepository.java
+++ AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Termo/TermoRepository.java
@@ -1,5 +1,13 @@
 package com.Equipe1.AssinaturaDigital.Termo;
 
-public class TermoRepository {
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface TermoRepository extends MongoRepository<TermoModel,String>{
 
+    Optional<TermoModel> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<TermoModel> findByNomeContainingIgnoreCase(String parteDoNome);
 }
