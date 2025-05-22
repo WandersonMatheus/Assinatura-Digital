c69eb1253b40a99796b16d5d8e62746b3e10aa4d
Wanderson Matheus <w.matheuspontesdev@gmail.com>
2025-05-22 13:53:03 -0300

    Feat: Finalized Cenario Repositorie class

--- AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Cenario/CenarioRepository.java
+++ AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Cenario/CenarioRepository.java
@@ -1,5 +1,10 @@
 package com.Equipe1.AssinaturaDigital.Cenario;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+@Repository
 public interface CenarioRepository extends MongoRepository<CenarioModel,String>{
+    List<CenarioModel> findByTitulo(String titulo);
 }
