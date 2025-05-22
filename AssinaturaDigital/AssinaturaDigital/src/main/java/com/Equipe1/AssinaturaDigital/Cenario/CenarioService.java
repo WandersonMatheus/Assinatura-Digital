995e578e4609dd5be5915cbd54d0e265c99d0da0
Wanderson Matheus <w.matheuspontesdev@gmail.com>
2025-05-22 14:32:33 -0300

    Feat: finalized Cenario Service class

--- AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Cenario/CenarioService.java
+++ AssinaturaDigital/AssinaturaDigital/src/main/java/com/Equipe1/AssinaturaDigital/Cenario/CenarioService.java
@@ -1,5 +1,47 @@
 package com.Equipe1.AssinaturaDigital.Cenario;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.Equipe1.AssinaturaDigital.Cliente.ClienteModel;
+
+@Service
 public class CenarioService {
 
+    private CenarioRepository cenarioRepository;
+
+    public CenarioService(CenarioRepository cenarioRepository) {
+        this.cenarioRepository = cenarioRepository;
+    }
+
+    //criar cenario
+    public CenarioModel criarCenario(CenarioModel cenario){
+        return cenarioRepository.save(cenario);
+    }
+    //Editar cenario
+    public Optional<CenarioModel> updateCenario(String id,CenarioModel dadosAtualizados){
+        return cenarioRepository.findById(id)
+            .map(cenario ->{
+                cenario.setTitulo(dadosAtualizados.getTitulo());
+                return cenarioRepository.save(cenario);
+            });
+    }
+    //excluir cenario
+    public boolean apagarCenario(String id){
+        if (cenarioRepository.existsById(id)) {
+            cenarioRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+    //buscar cenario 
+    public Optional<CenarioModel> buscarPorId(String id){
+        return cenarioRepository.findById(id);
+    }
+    //Listar cenario
+    public List<CenarioModel> listarCenarios(){
+        return cenarioRepository.findAll();
+    }
 }
