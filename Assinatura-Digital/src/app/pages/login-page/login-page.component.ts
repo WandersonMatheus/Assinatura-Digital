4b518d2cc0d2748a1bfd9f0f14d697740250fb10
Wanderson Matheus <w.matheuspontesdev@gmail.com>
2025-05-30 12:13:50 -0300

    fix: apaguei o que tinha do forms e refiz,por enquanto criei o controll em login page,agora falta um componente pra poder reaproveitar depois

--- Assinatura-Digital/src/app/pages/login-page/login-page.component.ts
+++ Assinatura-Digital/src/app/pages/login-page/login-page.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormControl
+} from '@angular/forms';
 import { LoginLayoutComponent } from '../../layout/login-layout/login-layout.component';
-import { PrimaryinputComponent } from '../../components/primaryinput/primaryinput.component';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginLayoutComponent,PrimaryinputComponent,ReactiveFormsModule],
+  imports: [LoginLayoutComponent,ReactiveFormsModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
   loginForm!: FormGroup;
 
-  constructor(){
+  constructor() {
     this.loginForm = new FormGroup({
-      email: new FormControl("", [Validators.required, Validators.email]),
-      password:new FormControl("", [Validators.required, Validators.minLength(6)])
-    })
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
   }
-
-
 }
