68e329be424160285b7d5100d0abd5a4b6fc6038
Wanderson Matheus <w.matheuspontesdev@gmail.com>
2025-06-11 14:43:29 -0300

    feat:preparei a tela de criar assinatura

--- Assinatura-Digital/src/app/pages/ass-create/ass-create.component.ts
+++ Assinatura-Digital/src/app/pages/ass-create/ass-create.component.ts
@@ -1,11 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ass-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './ass-create.component.html',
   styleUrl: './ass-create.component.scss'
 })
 export class AssCreateComponent {
+  form: FormGroup;
+  clientes: any[] = [];
+  termos: any[] = [];
+  cenarios: any[] = [];
+  linkGerado: string | null = null;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      clienteId: [''],
+      termoId: [''],
+      cenarioId: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.http.get('/api/clientes').subscribe((res: any) => this.clientes = res);
+    this.http.get('/api/termos').subscribe((res: any) => this.termos = res);
+    this.http.get('/api/cenarios').subscribe((res: any) => this.cenarios = res);
+  }
+
+  gerarLink() {
+    const payload = this.form.value;
+    this.http.post('/api/assinaturas/criar', payload).subscribe((res: any) => {
+      this.linkGerado = res.linkAssinatura;
+    });
+  }
+
+  copiarLink() {
+    if (this.linkGerado) {
+      navigator.clipboard.writeText(this.linkGerado);
+      alert('Link copiado para a área de transferência!');
+    }
+  }
 }
